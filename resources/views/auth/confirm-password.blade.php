0f5b1fb6543335562edccf2fbe967c35dacac12f
brice <brice.bousekkine@epitech.eu>
2021-03-05 14:48:51 +0100

    page profil with crud for user to cahnge info ok

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,36 +1,23 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('front.layout')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+@section('main')
+    <div class="row row-x-center s-styles">
+        <div class="column large-6 tab-12">          
+
+            <p class="h-add-bottom">@lang('This is a secure area of the application. Please confirm your password before continuing.')</p>
+
+            <!-- Validation Errors -->
+            <x-auth.validation-errors :errors="$errors" />
+
+            <form class="h-add-bottom" method="POST" action="{{ route('password.confirm') }}">
+                @csrf                
+
+                <!-- Password -->
+                <x-auth.input-password />
+
+                <x-auth.submit title="Confirm" />              
+            </form>
         </div>
+    </div>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.confirm') }}">
-            @csrf
-
-            <!-- Password -->
-            <div>
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <div class="flex justify-end mt-4">
-                <x-button>
-                    {{ __('Confirm') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@endsection
